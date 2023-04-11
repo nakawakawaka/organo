29a3a4fb6d4da04d1212e20d7c6b4e94b00d2a02
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2023-04-11 22:57:22 +0900

    criando funcionalidade de deletar colaborador

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
@@ -8,30 +9,37 @@ import Rodape from './componentes/Rodape';
 function App() {
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Programação',
       cor: '#57C278',
     },
     {
+      id: uuidv4(),
       nome: 'Front-end',
       cor: '#82CFFA',
     },
     {
+      id: uuidv4(),
       nome: 'Data Science',
       cor: '#A6D157',
     },
     {
+      id: uuidv4(),
       nome: 'Devops',
       cor: '#E06B69',
     },
     {
+      id: uuidv4(),
       nome: 'UX e Design',
       cor: 'D86EBF',
     },
     {
+      id: uuidv4(),
       nome: 'Mobile',
       cor: '#FEBA05',
     },
     {
+      id: uuidv4(),
       nome: 'Inovação e Gestã',
       cor: '#FF8A29',
     },
@@ -39,11 +47,19 @@ function App() {
 
   const inicial = [ 
     {
-    nome: 'naoki',
-    cargo: 'senhor',
+    id: uuidv4(),
+    nome: 'Naoki Nakamura',
+    cargo: 'Desenvolvedor Front-end',
     imagem: `https://github.com/nakawakawaka.png`,
-    time: 'Front-end'
-    }
+    time: times[1].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'JULIANA AMOASEI',
+      cargo: 'Desenvolvedora de software e instrutora',
+      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      time: times[0].nome
+    },
   ]
 
   const [colaboradores, setColaboradores] = useState(inicial)
@@ -53,13 +69,13 @@ function App() {
     setColaboradores([...colaboradores, colaborador]);
   }
 
-  const deletarColaborador = () => {
-    console.log('deletando colaborador')
+  const deletarColaborador = (id) => {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
   }
 
-  const mudarCorDoTime = (cor, nome) => {
+  const mudarCorDoTime = (cor, id) => {
     setTimes(times.map(time => {
-      if(time.nome === nome) time.cor = cor
+      if(time.id === id) time.cor = cor
       return time;
     }));
   }
@@ -70,9 +86,8 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
       
       {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        cor={time.cor} 
+        key={time.id} 
+        time={time}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         aoDeletar={deletarColaborador}
         mudarCor={mudarCorDoTime}
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,16 +2,21 @@ import Colaborador from '../Colaborador';
 import hextoRgba from 'hex-to-rgba';
 import './Time.css';
 
-const Time = (props) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
   return (
-    props.colaboradores.length > 0 && <section className='time' style={{ backgroundColor: hextoRgba(props.cor, 0.6) }}>
-      <input value={props.cor} onChange={evento => props.mudarCor(evento.target.value, props.nome)} type='color' className='input-cor' />
-      <h3 style={{borderColor:  props.cor}}>{props.nome}</h3>
+    colaboradores.length > 0 && <section className='time' style={{ backgroundColor: hextoRgba(time.cor, 0.6) }}>
+      <input value={time.cor} onChange={evento => mudarCor(evento.target.value, time.id)} type='color' className='input-cor' />
+      <h3 style={{borderColor:  time.cor}}>{time.nome}</h3>
 
       <div className='colaboradores'>
-      {props.colaboradores.map( colaborador => {
-        
-        return <Colaborador key={colaborador.nome} corDeFundo={props.cor} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={props.aoDeletar} />
+      {colaboradores.map( colaborador => {
+        return <Colaborador 
+                  key={colaborador.id} 
+                  colaborador={colaborador}
+                  corDeFundo={time.cor} 
+                  aoDeletar={aoDeletar} 
+
+                />
       } )}
       </div>
     </section>
